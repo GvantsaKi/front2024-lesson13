daecacd07e1a4173eb6c9fba90424fe008009e02
GvantsaK <gvancaa99@gmail.com>
2024-03-31 19:14:59 +0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let myName="Gvantsa";
+let myHobby="History";
+let yearOfBirth="1999";
+let currentYear="2024";
+
+
+console.log(myName);
+console.log(myHobby);
+console.log(yearOfBirth);
+console.log(currentYear);
